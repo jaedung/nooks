2107e6f2bfe7c0baa853e50381a8bc421cf0e349
justin <justinkim0310@gmail.com>
2019-12-12 13:00:30 +1000

    class component state vs hook state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,41 @@ import React, { useState } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
       <p>{count}</p>
     </div>
   );
 };
+
+// class App extends React.Component {
+//   state = {
+//     count: 0
+//   };
+
+//   increment = () => {
+//     this.setState({ count: this.state.count + 1 });
+//   };
+
+//   decrement = () => {
+//     this.setState({ count: this.state.count - 1 });
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <button onClick={this.increment}>Increment</button>
+//         <button onClick={this.decrement}>Decrement</button>
+//         <p>{this.state.count}</p>
+//       </>
+//     );
+//   }
+// }
 
 export default App;
